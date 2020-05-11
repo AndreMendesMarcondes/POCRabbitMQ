904ce48ddc1617876df9d8489d5c766c6a820bee
André Mendes Marcondes <andremendesmarcondes@gmail.com>
2020-05-10 22:43:24 -0300

    Add project files.

---
+++ POCRabbitMQ/Controllers/RabbitController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POCRabbitMQ.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace POCRabbitMQ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RabbitController : ControllerBase
+    {
+        private IRabbitManager _manager;
+
+        public RabbitController(IRabbitManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var num = new System.Random().Next(9000);
+
+            _manager.Publish(new
+            {
+                field1 = $"PocRabbitMQ-{num}",
+                field2 = $"Mineiro-{num}"
+            }, "demo.exchange.topic.dotnetcore", "topic", "*.queue.durable.dotnetcore.#");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ POCRabbitMQ/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ObjectPool;
+using POCRabbitMQ.Engine;
+using POCRabbitMQ.Models;
+using POCRabbitMQ.Services;
+using POCRabbitMQ.Services.Interfaces;
+using RabbitMQ.Client;
+
+namespace POCRabbitMQ
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var rabbitConfig = Configuration.GetSection("rabbit");
+            services.Configure<RabbitOptions>(rabbitConfig);
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<IPooledObjectPolicy<IModel>, RabbitModelPooledObjectPolicy>();
+
+            services.AddSingleton<IRabbitManager, RabbitManager>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
